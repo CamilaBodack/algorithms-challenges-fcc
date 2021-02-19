cf190837ca5fc46b0a1ddaf9468dad96ec55c5cc
camilabodack <camila.bodack@gmail.com>
2021-02-19 10:50:27 -0300

    add solution to find symmetric difference when input is equal to two arrays

--- algorithms/find_symmetric_difference.js
+++ algorithms/find_symmetric_difference.js
@@ -5,3 +5,18 @@ The mathematical term symmetric difference (△ or ⊕) of two sets is the set o
 Symmetric difference is a binary operation, which means it operates on only two elements. So to evaluate an expression involving symmetric differences among three elements (A △ B △ C), you must complete one operation at a time. Thus, for sets A and B above, and C = {2, 3}, A △ B △ C = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}.
 
 */
+
+function sym(args) {
+  let arrgs = Array.prototype.slice.call(arguments);
+  let mergeArrays = arrgs[0].concat(arrgs[1])
+  console.log(mergeArrays)
+  let symmetricArr = []
+    mergeArrays.forEach(function(i){
+      if(!(arrgs[0].includes(i) && arrgs[1].includes(i))){
+        symmetricArr.push(i)
+      }
+    })
+    return symmetricArr
+  }
+
+sym([1, 2, 3], [5, 2, 1, 4]);
