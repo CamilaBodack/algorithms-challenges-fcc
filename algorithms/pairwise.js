6895a04e587fb7bea5038076f910024c7c8cd06f
Camila Bodack <32226250+CamilaBodack@users.noreply.github.com>
2022-01-06 21:30:47 -0300

    Update pairwise logic

--- algorithms/pairwise.js
+++ algorithms/pairwise.js
@@ -2,24 +2,44 @@
 Given an array arr, find element
 pairs whose sum equal the second argument
 arg and return the sum of their indices.
+
+WIP
 */
 
+
 function pairwise(arr, arg) {
-    let result = 0
-    let item = 0
-    let sub = 0
+    let item = 0;
+    let proximo = 0;
+    let soma = 0;
+    let pairs = [];
+    let sumIndexs = 0;
+    let pair = []
+    if(arr.length === 0){return 0}
     for(let i = 0; i < arr.length; i++){
-        item = arr[i]
-        if(arr.length === 0){return 0}
-        while(result !== arg){
-            sub = arg - item
-            result = sub + item
+        for(let j = 0; j < arr.length; j++){
+            item = arr[i]
+            proximo = arr[j]
+            soma = item + proximo
+            if(soma === arg){
+                pair = [item, proximo]
+                if(pairs.includes(pair || pair.reverse())){continue}
+                pairs.push(pair)
+                }   
+                
+            }
+                
         }
-     return arr.indexOf(sub) + arr.indexOf(item)
- 
- 
-    }
- }
+    
+    pairs.forEach(item => {
+        sumIndexs += arr.indexOf(item)
+    })
+
+    return pairs
+}
+
+console.log(pairwise([1, 3, 2, 4], 4)) 
+//console.log(pairwise([1, 4, 2, 3, 0, 5], 7))
+//console.log(pairwise([7, 9, 11, 13, 15], 20));
  
  
  console.log(pairwise([7, 9, 11, 13, 15], 20))
